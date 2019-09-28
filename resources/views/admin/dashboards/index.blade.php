ca94ad0bb50ab4499a289074782e82b8ace0a122
Praibool Namwong <praiboolnamwong@Praibools-MacBook-Pro.local>
2019-09-28 16:40:14 +0700

    create menu

--- resources/views/admin/dashboards/index.blade.php
+++ resources/views/admin/dashboards/index.blade.php
@@ -127,7 +127,7 @@
                                 <span class="direct-chat-timestamp float-right">23 Jan 2:00 pm</span>
                             </div>
                             <!-- /.direct-chat-infos -->
-                            <img class="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image">
+                            <img class="direct-chat-img" src="{{ asset('lte-design/dist/img/user1-128x128.jpg') }}" alt="message user image">
                             <!-- /.direct-chat-img -->
                             <div class="direct-chat-text">
                                 Is this template really for free? That's unbelievable!
@@ -143,7 +143,7 @@
                                 <span class="direct-chat-timestamp float-left">23 Jan 2:05 pm</span>
                             </div>
                             <!-- /.direct-chat-infos -->
-                            <img class="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image">
+                            <img class="direct-chat-img" src="{{ asset('lte-design/dist/img/user3-128x128.jpg') }}" alt="message user image">
                             <!-- /.direct-chat-img -->
                             <div class="direct-chat-text">
                                 You better believe it!
@@ -159,7 +159,7 @@
                                 <span class="direct-chat-timestamp float-right">23 Jan 5:37 pm</span>
                             </div>
                             <!-- /.direct-chat-infos -->
-                            <img class="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image">
+                            <img class="direct-chat-img" src="{{ asset('lte-design/dist/img/user1-128x128.jpg') }}" alt="message user image">
                             <!-- /.direct-chat-img -->
                             <div class="direct-chat-text">
                                 Working with AdminLTE on a great new app! Wanna join?
@@ -175,7 +175,7 @@
                                 <span class="direct-chat-timestamp float-left">23 Jan 6:10 pm</span>
                             </div>
                             <!-- /.direct-chat-infos -->
-                            <img class="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image">
+                            <img class="direct-chat-img" src="{{ asset('lte-design/dist/img/user3-128x128.jpg') }}" alt="message user image">
                             <!-- /.direct-chat-img -->
                             <div class="direct-chat-text">
                                 I would love to.
@@ -192,7 +192,7 @@
                         <ul class="contacts-list">
                             <li>
                                 <a href="#">
-                                    <img class="contacts-list-img" src="dist/img/user1-128x128.jpg">
+                                    <img class="contacts-list-img" src="{{ asset('lte-design/dist/img/user1-128x128.jpg') }}">
 
                                     <div class="contacts-list-info">
                           <span class="contacts-list-name">
@@ -207,7 +207,7 @@
                             <!-- End Contact Item -->
                             <li>
                                 <a href="#">
-                                    <img class="contacts-list-img" src="dist/img/user7-128x128.jpg">
+                                    <img class="contacts-list-img" src="{{ asset('lte-design/dist/img/user7-128x128.jpg') }}">
 
                                     <div class="contacts-list-info">
                           <span class="contacts-list-name">
@@ -222,7 +222,7 @@
                             <!-- End Contact Item -->
                             <li>
                                 <a href="#">
-                                    <img class="contacts-list-img" src="dist/img/user3-128x128.jpg">
+                                    <img class="contacts-list-img" src="{{ asset('lte-design/dist/img/user3-128x128.jpg') }}">
 
                                     <div class="contacts-list-info">
                           <span class="contacts-list-name">
@@ -237,7 +237,7 @@
                             <!-- End Contact Item -->
                             <li>
                                 <a href="#">
-                                    <img class="contacts-list-img" src="dist/img/user5-128x128.jpg">
+                                    <img class="contacts-list-img" src="{{ asset('lte-design/dist/img/user5-128x128.jpg') }}">
 
                                     <div class="contacts-list-info">
                           <span class="contacts-list-name">
@@ -252,7 +252,7 @@
                             <!-- End Contact Item -->
                             <li>
                                 <a href="#">
-                                    <img class="contacts-list-img" src="dist/img/user6-128x128.jpg">
+                                    <img class="contacts-list-img" src="{{ asset('lte-design/dist/img/user6-128x128.jpg') }}">
 
                                     <div class="contacts-list-info">
                           <span class="contacts-list-name">
@@ -267,7 +267,7 @@
                             <!-- End Contact Item -->
                             <li>
                                 <a href="#">
-                                    <img class="contacts-list-img" src="dist/img/user8-128x128.jpg">
+                                    <img class="contacts-list-img" src="{{ asset('lte-design/dist/img/user8-128x128.jpg') }}">
 
                                     <div class="contacts-list-info">
                           <span class="contacts-list-name">
