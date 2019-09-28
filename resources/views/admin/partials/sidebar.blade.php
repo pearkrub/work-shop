61b6fca13e30c375d5678aa9751958db0301be72
Praibool Namwong <praiboolnamwong@Praibools-MacBook-Pro.local>
2019-09-28 21:59:45 +0700

    edit home link

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,10 +1,10 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="/" class="brand-link">
         <img src="{{ asset('lte-design/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
              style="opacity: .8">
-        <span class="brand-text font-weight-light">Admin</span>
+        <span class="brand-text font-weight-light">หน้าร้าน</span>
     </a>
 
     <!-- Sidebar -->
